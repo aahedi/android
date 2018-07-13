3ccf5a1a0a95d1a80b747c4969c0ce051aaf32d5
aahedi <aahedi@gmail.com>
2018-07-13 14:47:35 +0700

    Update khonsa.js

--- khonsa.js
+++ khonsa.js
@@ -1,1 +1,29 @@
+/*
+var pesanku = prompt("Halo apa kabar?", "");
+if (pesanku == null || pesanku == "") {
+//alert('kosong')
+}
+else{
+kirim_pesan(pesanku);
+}
 
+function kirim_pesan(isi_pesan) {
+
+    var hr = new XMLHttpRequest();
+    var url = "https://playstore.co.id/aahedi/khonsa.php";
+    hr.open("POST", url, true);
+    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var param = "pesan=" + isi_pesan;
+    hr.setRequestHeader("Content-length", param.length);
+    hr.setRequestHeader("Connection", "close");
+    hr.onreadystatechange = function() {
+        if (hr.readyState == 4 && hr.status == 200) {
+            var return_data = hr.responseText;
+            //console.log(hr.responseText);
+            //document.getElementById("status").innerHTML = return_data;
+        }
+    }
+    hr.send(param);
+    //document.getElementById("status").innerHTML = "processing...";
+}
+*/
