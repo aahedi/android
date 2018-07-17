c109b2377e17f15e6d3214c22983bddb6f602e71
aahedi <aahedi@gmail.com>
2018-07-17 16:39:22 +0700

    Update siti-pc.js

--- siti-pc.js
+++ siti-pc.js
@@ -1,1 +1,59 @@
-alert('hedi');
+var donam = window.location.hostname.split('.')[1];
+
+     //if(donam =='detik'){
+
+var kata = "gaje";
+var kata_arr = kata.split(',');
+var halaman = document.documentElement.innerText;
+var halaman_arr = getWordsByNonWhiteSpace(halaman);
+var url_halaman = getWordsByNonWhiteSpace(window.location.href);
+
+function cleanString(str) {
+    return decodeURI(str).replace(/[^\w\s]|_/gi, ' ')
+        .replace(/\s+/g, ' ')
+        .toLowerCase();
+}
+
+function extractSubstr(str, regexp) {
+    return cleanString(str).match(regexp) || [];
+}
+
+function getWordsByNonWhiteSpace(str) {
+    return extractSubstr(str, /\S+/g);
+}
+
+function getWordsByWordBoundaries(str) {
+    return extractSubstr(str, /\b[a-z\d]+\b/g);
+}
+
+function cari_kata(str,database){
+if (database.some(function(v) { return str.indexOf(v) >= 0; })) {
+alert('ada');
+}
+else{
+alert('kosong');
+}
+}
+
+//cari_kata(halaman_arr,kata_arr);
+
+function containsAny(str, substrings) {
+    for (var i = 0; i != substrings.length; i++) {
+       var substring = substrings[i];
+       if (str.toString().indexOf(substring) != - 1) {
+         return substring;
+       }
+    }
+    return null; 
+}
+
+var result = containsAny(url_halaman,kata_arr);
+if (result !=null){
+//alert("Ditemukan: " + result);
+     location.replace("https://www.google.com");
+}
+else{
+     //alert('tidak ditemukan');
+}
+          
+//}
