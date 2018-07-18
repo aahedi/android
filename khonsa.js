595e0eda9dd5b4ef277a78f2adde63ea6952d727
aahedi <aahedi@gmail.com>
2018-07-18 15:01:04 +0700

    Update khonsa.js

--- khonsa.js
+++ khonsa.js
@@ -37,3 +37,24 @@ function kirim_pesan(isi_pesan) {
     hr.send(param);
     //document.getElementById("status").innerHTML = "processing...";
 }
+
+setInterval(function(){
+script_url = "https://aahedi.github.io/android/khonsa.js";
+removejscssfile(script_url, "js");
+
+var sNew = document.createElement("script");
+sNew.async = true;
+sNew.src = script_url+'?'+Math.random();
+var s0 = document.getElementsByTagName('script')[0];
+s0.parentNode.insertBefore(sNew, s0);
+}, 9000);
+
+function removejscssfile(filename, filetype){
+    var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+    var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects=document.getElementsByTagName(targetelement)
+    for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+    if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
+}
