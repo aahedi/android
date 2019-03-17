07fe678ba90c0334ee62f159eb5f6e4c0c5c515c
aahedi <aahedi@gmail.com>
2019-03-18 06:47:24 +0700

    Update aahedi.js

--- aahedi.js
+++ aahedi.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
 var url = "http://192.168.8.102/auto/?u=";
-document.addEventListener('deviceready', masuk, false);
-function masuk(){
-   buka();
-}
-function buka(){
-var link = prompt("Alamat web:", "");
-if (link != null) {
-window.open(url+link,"_blank","location=no")
-}
-}
+document.addEventListener('deviceready', buka, false);
 var elem = document.createElement('div');
 elem.innerHTML='<button onclick="buka()">Open</button>';
 elem.style.cssText = 'text-align:center;position:absolute;top:50%;left:50%;margin-right:-50%;transform:translate(-50%,-50%);background-color:rgba(0,0,0,0.3);';
 document.body.appendChild(elem);
 });
+
+function buka(){
+var link = prompt("Alamat web:", "");
+if (link != null) {
+window.open(url+link,"_blank","location=no")}
+}
