54d9b02a8333b473f488e9813597c455aab5fd0f
aahedi <aahedi@gmail.com>
2019-03-18 06:21:42 +0700

    Update aahedi.js

--- aahedi.js
+++ aahedi.js
@@ -4,7 +4,7 @@ document.addEventListener('deviceready', masuk, false);
 function masuk(){
   //setTimeout(function(){window.open('"+url+"','_blank')},3e3)
 }
-var elem = document.createElement(div);
+var elem = document.createElement('div');
 elem.innerHTML='<a href='+url+'>'+url+'</a>';
 elem.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000';
 document.body.appendChild(elem);
