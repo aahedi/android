d349c6d0cab43940791b601391989f852b65dd38
aahedi <aahedi@gmail.com>
2019-03-19 15:45:43 +0700

    Update aahedi.js

--- aahedi.js
+++ aahedi.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-document.addEventListener('deviceready', buka, false);
+document.addEventListener('deviceready', siap, false);
 var elem = document.createElement('div');
-elem.innerHTML='<button onclick="buka()">Open</button>';
+elem.innerHTML='<input type="url" placeholder="url" id="url"/><br><input type="text" placeholder="image keyword" id="keyword"/><br><button onclick="buka()" style="height:25px;">Open</button>';
 elem.style.cssText = 'text-align:center;position:absolute;top:50%;left:50%;margin-right:-50%;transform:translate(-50%,-50%);background-color:rgba(0,0,0,0.3);';
 document.body.appendChild(elem);
 });
 
 function buka(){
-var url = "http://192.168.100.3/auto/?u=";
+var base_url = "http://192.168.100.3/auto/?u=";
+var url = document.getElementById('url');
+var keyword = document.getElementById('keyword');
+
+window.open(base_url+url.value+"?&s="+keyword.value,"_blank","location=no")}
+/*
 var link = prompt("Alamat web:", "");
 if (link != null) {
 window.open(url+link,"_blank","location=no")}
+*/
+}
+
+function siap(){
 }
